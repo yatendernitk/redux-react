a08c6bb58a8136fe025faf8869a6aa819f2e4dc3
ok <ok@yimac.local>
2018-03-04 21:21:05 +0530

    state change with more function

--- main4.js
+++ main4.js
@@ -10,9 +10,20 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id === action.id) {
+                    return {
+                        ...todo,
+                           completed: !todo.completed
+                    }
+                }else {
+                    return todo;
+                }
+            });
         default:
-            break;
-    }
+        break;
+    };
 };
 
 const testAddTodo = () => {
@@ -37,5 +48,45 @@ const testAddTodo = () => {
     ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: "learn Redux",
+            completed: false
+        },
+        {
+            id: 1,
+            text: "Go Shopping",
+            completed: true
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+
+    const stateAfter = [
+        {
+            id: 0,
+            text: "learn Redux",
+            completed: false
+        },
+        {
+            id: 1,
+            text: "Go Shopping",
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('all tests passed');
