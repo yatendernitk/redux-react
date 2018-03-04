92084094a52e31ec1e82e60f32ece0569f6543f7
ok <ok@yimac.local>
2018-03-04 21:33:55 +0530

    adding states

--- main4.js
+++ main4.js
@@ -1,26 +1,29 @@
 'use strict';
+const todo = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id){
+                return state;
+            }else{
+                return {...state, completed: !state.completed}
+            };
+        default:
+            return state;
+    }
+}
+
 const todos = (state = [], action) => {
     switch (action.type) {
         case 'ADD_TODO':
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ];
+            return [...state, todo(undefined, action)];
         case 'TOGGLE_TODO':
-            return state.map(todo => {
-                if (todo.id === action.id) {
-                    return {
-                        ...todo,
-                           completed: !todo.completed
-                    }
-                }else {
-                    return todo;
-                }
-            });
+            return state.map(t => todo(t, action));
         default:
         break;
     };
